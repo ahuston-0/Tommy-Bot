63f1d39ac1dcd259acbb9f8ad6124c8e18286eda
Alice Huston <shuston@stevens.edu>
2019-08-18 18:46:08 -0400

    Added JSON config file and git update functionality

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
+const fs = require("fs")
 const Discord = require('discord.js');
 const logger = require('winston');
 const auth = require('./auth.json');
-let command_var = '!';
+const s_git = require('simple-git');
+let config = require('./config.json');
+let command_var = config.command_var;
 
 //Configure logger
 logger.remove(logger.transports.Console);
@@ -14,8 +17,8 @@ logger.level = 'debug';
 const bot = new Discord.Client();
 const bot_return = bot.login(auth.token);
 bot.once('ready', () => {
-    logger.info(bot_return.toString());
     logger.info('Connected!');
+    logger.info("Logged in as " + bot.user.username + "-" + bot.user.discriminator);
 });
 
 
@@ -25,16 +28,15 @@ bot.on('message', (message)=>{
     //The current default is '!'
     if (message.content.substring(0,1) === command_var){
         let args = message.content.substring(1).split(" ");
-        const cmd = args[0];
+        let cmd = args[0];
 
-        args = args.splice(1);
-        logger.info(args);
         switch (cmd){
             case 'ping':
                 message.channel.send('pong')
                 break;
             case 'update':
-                message.channel.send('Shutting down to update')
+                message.channel.send('Shutting down to update');
+                s_git.pull("origin/master", "master");
                 break;
             case 'srccode':
                 message.channel.send('Find my source code here! https://github.com/ahuston-0/Tommy-Bot')
@@ -47,9 +49,21 @@ bot.on('message', (message)=>{
                     "WIP\n"+
                     "update")
                 break;
-            case 'kill':
-                message.channel.send('Agh you got me! Goodbye.');
-                return process.exit(1);
+            case 'reboot':
+                message.channel.send('Resetting...')
+                    .then(msg => bot.destroy())
+                    .then(() => bot.login(auth.token));
+                break;
+            case 'cmd_var':
+                command_var = args[1];
+                config.command_var = command_var;
+                fs.writeFile("./config.json", JSON.stringify(config, null, 4), (err) =>{
+                    if (err){
+                        logger.error(err);
+                    }
+                });
+                message.channel.send('Command prefix changed to ' + command_var);
+                break;
             default:
                 break;
         }
