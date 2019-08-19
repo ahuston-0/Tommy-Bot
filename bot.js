56bad5593e5506f1cb799c8f9ea4059767c93cf2
Alice Huston <shuston@stevens.edu>
2019-08-18 20:22:57 -0400

    Added welcome messages, changed help message

--- bot.js
+++ bot.js
@@ -2,7 +2,8 @@ const fs = require("fs")
 const Discord = require('discord.js');
 const logger = require('winston');
 const auth = require('./auth.json');
-const s_git = require('simple-git');
+const s_git_promise = require('simple-git/promise')();
+const welcome_msgs = require('./welcome_messages.json');
 let config = require('./config.json');
 let command_var = config.command_var;
 
@@ -15,7 +16,7 @@ logger.level = 'debug';
 
 //Initialize bot
 const bot = new Discord.Client();
-const bot_return = bot.login(auth.token);
+bot.login(auth.token);
 bot.once('ready', () => {
     logger.info('Connected!');
     logger.info("Logged in as " + bot.user.username + "-" + bot.user.discriminator);
@@ -28,26 +29,34 @@ bot.on('message', (message)=>{
     //The current default is '!'
     if (message.content.substring(0,1) === command_var){
         let args = message.content.substring(1).split(" ");
-        let cmd = args[0];
+        let cmd = args[0].toLocaleLowerCase();
 
         switch (cmd){
             case 'ping':
-                message.channel.send('pong')
+                message.channel.send('pong');
                 break;
             case 'update':
-                message.channel.send('Shutting down to update');
-                s_git().pull("origin","master", {'--rebase':true});
+                message.channel.send('Pulling latest updates from GitHub');
+                s_git_promise.pull().then((success) => {
+                    message.channel.send('Update successful, please restart me!')
+                }, (failed) => {
+                    message.channel.send('Update failed, please check logs')
+                });
                 break;
             case 'srccode':
-                message.channel.send('Find my source code here! https://github.com/ahuston-0/Tommy-Bot')
+                message.channel.send('Find my source code here! https://github.com/ahuston-0/Tommy-Bot');
                 break;
             case 'help':
-                message.channel.send("Command List\n" +
-                    "To execute commands, call the bot with " + command_var + "< command >\n\n" +
-                    "Fully Functional\n" +
-                    "help, ping, srccode\n\n" +
-                    "WIP\n"+
-                    "update")
+                message.channel.send("**Command List**\n" +
+                    "To execute commands, call the bot with " + command_var + "command\n\n" +
+                    "**All Users**\n" +
+                    "Help - This list of commands\n" +
+                    "Ping - A little game the dev and I use to test\n" +
+                    "SrcCode - A link to my source code!\n\n" +
+                    "**Admins**\n"+
+                    `Cmd_Var - Change the command prefix from ${command_var}\n`+
+                    "Reboot - Soft reboot me\n" +
+                    "Update - Pull the latest version of me from GitHub");
                 break;
             case 'reboot':
                 message.channel.send('Resetting...')
@@ -68,4 +77,14 @@ bot.on('message', (message)=>{
                 break;
         }
     }
+});
+
+bot.on('guildMemberAdd', member => {
+   const channel = member.guild.channels.find(ch => ch.name === 'general');
+   const keys = Object.keys(welcome_msgs);
+   const rand_index = Math.floor(Math.random() * keys.length);
+   const rand_key = keys[rand_index];
+   const message = welcome_msgs[rand_key];
+   if(!channel) return;
+   channel.send(message + `Welcome to the server, ${member}!`);
 });
