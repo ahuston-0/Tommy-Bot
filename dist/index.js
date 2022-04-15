2c6cc9d08b87f2425a978e9b5af106fbf7707bb6
ahuston-0 <aliceghuston@gmail.com>
2022-04-15 00:48:55 -0400

    ci(tsc): test pre-commit hook for tsc
    lint-staged does not save any changes to dist/ (or emit anything for
    that matter). Therefore, any changes have to get picked up after running
    yarn compile. The current pipeline is lint-staged -> tsc -> stage dist/.

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ require("dotenv/config");
 const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
 client.once('ready', () => {
     console.log('TommyBot is ready!');
+    console.log('lint');
 });
 void client.login(process.env.token);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.once('ready', () => {
   console.log('TommyBot is ready!');
+  console.log('lint');
 });
 
 void client.login(process.env.token);
